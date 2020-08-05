5c74a91bf9bbfc98ce112d42420a6466cf6d5c0a
Kristian Freeman <kristian@kristianfreeman.com>
2020-08-05 10:12:27 -0500

    Conditional blue/gray state for subsection badges (#74)
    * Conditional blue/gray state for subsection badges
    
    Closes #65
    
    * Correct active subsection coloring/logic

--- app/src/components/subsection.js
+++ app/src/components/subsection.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-export default ({ active, children, title }) => {
+export default ({ active, children, past = false, title }) => {
   const classes = `flex items-center justify-content mb-2`;
   return (
     <div className={`mt-6 ${active ? "" : "mb-8"}`}>
       <div className={classes}>
-        <div className="w-4 h-4 bg-blue-4 rounded-full -ml-12" />
+        <div
+          className={`w-4 h-4 ${
+            past || active ? "bg-blue-4" : "bg-white border-2 border-gray-3"
+          } rounded-full -ml-12`}
+        />
         <div className="ml-8">
           <span className="text-gray-1">{title}</span>
         </div>
