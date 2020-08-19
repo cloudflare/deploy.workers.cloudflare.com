842e81d4709517d805b4aad081c36ac5680e7197
Kristian Freeman <kristian@kristianfreeman.com>
2020-08-19 14:56:40 -0500

    Update templates

--- app/src/components/templates.js
+++ app/src/components/templates.js
@@ -2,11 +2,11 @@ import React from "react";
 import Embed from "./embed";
 
 const TEMPLATES = [
-  "https://github.com/bytesizedxyz/cloudflare-worker-cra",
-  "https://github.com/cherry/placeholders.dev",
-  "https://github.com/adamschwartz/web.scraper.workers.dev",
   "https://github.com/signalnerve/workers-graphql-server",
-  "https://github.com/wilsonzlin/edgesearch",
+  "https://github.com/bytesizedxyz/cloudflare-worker-cra",
+  "https://github.com/adamschwartz/web.scraper.workers.dev",
+  "https://github.com/cherry/placeholders.dev",
+  "https://github.com/davidtsong/worker-speedtest-template",
 ];
 
 export default () => (
