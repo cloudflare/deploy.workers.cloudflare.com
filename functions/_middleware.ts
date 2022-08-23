81f96efbe6e1d686ef77eea5315df6bbd40db999
Adam Janiš <eidam@cloudflare.com>
2022-08-23 10:21:01 +0100

    add sentry

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,7 +1,17 @@
-import sentryPlugin from "@cloudflare/pages-plugin-sentry";
+import sentryPlugin from '@cloudflare/pages-plugin-sentry';
 
 export const onRequest: PagesFunction<{
-  SENTRY_DSN: string;
-}> = (context) => {
-  return sentryPlugin({ dsn: context.env.SENTRY_DSN })(context);
+	SENTRY_DSN: string;
+	CF_ACCESS_CLIENT_ID: string;
+	CF_ACCESS_CLIENT_SECRET: string;
+}> = context => {
+	return sentryPlugin({
+		dsn: context.env.SENTRY_DSN,
+		transportOptions: {
+			headers: {
+				'CF-Access-Client-Id': context.env.CF_ACCESS_CLIENT_ID,
+				'CF-Access-Client-Secret': context.env.CF_ACCESS_CLIENT_SECRET,
+			},
+		},
+	})(context);
 };
