ceed405bf82a7119e37e08fdebb6d7318aa21f5a
Adam Janiš <eidam@cloudflare.com>
2022-08-30 11:36:25 +0100

    proxy /button to svg file

---
+++ functions/deploy.ts
@@ -0,0 +1,8 @@
+export const onRequest: PagesFunction = async context => {
+	const {
+		request, // same as existing Worker API
+		env, // same as existing Worker API
+	} = context;
+	const url = new URL(request.url)
+	return env.ASSETS.fetch(url.protocol + "//" + url.hostname + "/deploy.svg", request)
+};
